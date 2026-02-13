7c0d4b58ef96c6719d5d496ef30fb2680fa04308
gutek00714 <guciolaczkowski1@gmail.com>
2026-02-13 19:35:50 +0100

    Add healthz path

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	// create a new ServeMux (router)
 	mux := http.NewServeMux()
 
 	// serve files from the current directory at the root path
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	// set healthz path
+	mux.HandleFunc("/healthz", healthzHandlerFunction)
 
 	// create a Server struct with handler and addr
-	server := http.Server{
+	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
-	server.ListenAndServe()
+	log.Println("Serving on port: 8080")
+	log.Fatal((server.ListenAndServe()))
+
+	// server.ListenAndServe()
+}
+
+func healthzHandlerFunction(w http.ResponseWriter, r *http.Request) {
+	// set content-type header
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+
+	// set the status code
+	w.WriteHeader(http.StatusOK)
+
+	// write the body text
+	myBytes := []byte("OK")
+	w.Write(myBytes)
 }
