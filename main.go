095fedf8d5d423d9349f953262d5dc263480c75b
gutek00714 <guciolaczkowski1@gmail.com>
2026-02-15 17:00:44 +0100

    Add metrics and reset paths + functions

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	// create a new ServeMux (router)
 	mux := http.NewServeMux()
 
+	// initialize apiConfig
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	// serve files from the current directory at the root path
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	// mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	// set healthz path
 	mux.HandleFunc("/healthz", healthzHandlerFunction)
+
+	// set metrics path
+	mux.HandleFunc("/metrics", apiCfg.metricsHandlerFunction)
+
+	// set reset path
+	mux.HandleFunc("/reset", apiCfg.resetHandlerFunction)
 
 	// create a Server struct with handler and addr
 	server := &http.Server{
@@ -37,4 +51,24 @@ func healthzHandlerFunction(w http.ResponseWriter, r *http.Request) {
 	// write the body text
 	myBytes := []byte("OK")
 	w.Write(myBytes)
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandlerFunction(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetHandlerFunction(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
