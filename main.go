8bee67c4919510c3878f47bf7253035b3cb389dd
gutek00714 <guciolaczkowski1@gmail.com>
2026-02-17 19:20:16 +0100

    Add POST /api/validate_chirp endpoint with JSON helpers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,6 +29,9 @@ func main() {
 
 	// set reset path
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandlerFunction)
+
+	// set validate_chirp paht
+	mux.HandleFunc("POST /api/validate_chirp", validateHelperFunction)
 
 	// create a Server struct with handler and addr
 	server := &http.Server{
@@ -78,4 +82,60 @@ func (cfg *apiConfig) metricsHandlerFunction(w http.ResponseWriter, r *http.Requ
 
 func (cfg *apiConfig) resetHandlerFunction(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+}
+
+func validateHelperFunction(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	// check if body is 140 or less characters
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+	} else {
+		respondWithJSON(w, 200, validResponse{Valid: true})
+	}
+}
+
+// helper functions
+type validResponse struct {
+	Valid bool `json:"valid"`
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	resp := errorResponse{Error: msg}
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
 }
